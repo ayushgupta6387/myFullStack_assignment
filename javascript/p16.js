7f130cc10a21aa59719f9102c579f39848dfdb27
ayushgupta6387 <53790277+ayushgupta6387@users.noreply.github.com>
2020-11-08 11:36:58 +0530

    format files

--- javascript/p16.js
+++ javascript/p16.js
@@ -3,18 +3,16 @@ var todoInput = document.querySelector(".todo-input");
 var todoButton = document.querySelector(".todo-button");
 var todoList = document.querySelector(".todo-list");
 
-
 // event handler
 todoButton.onclick = create;
 todoList.onclick = checkDelete;
 
-
 // function
-function create(e){
-    e.preventDefault();
-    if(todoInput.value != ""){
+function create(e) {
+  e.preventDefault();
+  if (todoInput.value != "") {
     var newDiv = document.createElement("div");
-    newDiv.classList.add('todo');
+    newDiv.classList.add("todo");
     var newLi = document.createElement("li");
     newLi.classList.add("todo-item");
     newLi.innerHTML = todoInput.value;
@@ -31,25 +29,23 @@ function create(e){
     newDiv.appendChild(deleteBtn);
 
     todoList.appendChild(newDiv);
-    }else{
-        alert("Do not include empty string!")
-    }
-    todoInput.value="";
-    
+  } else {
+    alert("Do not include empty string!");
+  }
+  todoInput.value = "";
 }
 
-function checkDelete(e){
-    // target will give where user clicked
-    var item = e.target;
-    if(item.classList[0]==="delete-btn"){
-        var parent = item.parentNode;
-        parent.remove();
-    }
+function checkDelete(e) {
+  // target will give where user clicked
+  var item = e.target;
+  if (item.classList[0] === "delete-btn") {
+    var parent = item.parentNode;
+    parent.remove();
+  }
 
-    if(item.classList[0]==="check-btn"){
-        var parent = item.parentNode;
-        // with toggle (remove and add both work)
-        parent.classList.toggle("completed");
-    }
-
+  if (item.classList[0] === "check-btn") {
+    var parent = item.parentNode;
+    // with toggle (remove and add both work)
+    parent.classList.toggle("completed");
+  }
 }
